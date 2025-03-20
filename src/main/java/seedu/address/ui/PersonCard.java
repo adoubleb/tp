07f018325b4725db721496ba40bbe0b7fdef08fe
nictjh <nicholastok0101@gmail.com>
2025-03-20 19:26:00 +0800

    Fix Checkstyle error

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -10,7 +10,6 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.Birthday;
-import seedu.address.model.person.Notes;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Relationship;
 
