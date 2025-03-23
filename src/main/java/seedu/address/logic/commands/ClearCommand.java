b0b3959db348fd4f132e12f02799165bef970a8a
adoubleb <hakam.9@outlook.com>
2025-03-23 19:12:51 +0800

    Setup clearcommand to implement confirmable

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -8,16 +8,29 @@ import seedu.address.model.Model;
 /**
  * Clears the address book.
  */
-public class ClearCommand extends Command {
+public class ClearCommand extends Command implements ConfirmableCommand {
 
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
-
+    public static final String MESSAGE_CONFIRMATION = "Are you sure you want to clear the address book?";
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.setAddressBook(new AddressBook());
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+    @Override
+    public String getConfirmationString() {
+        return MESSAGE_CONFIRMATION;
+    }
+
+    public CommandResult executeConfirmed(Model model) {
+        return execute(model);
+    }
+
+    public CommandResult executeAborted() {
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
