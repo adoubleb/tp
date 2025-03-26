70b083585dc6fdcedd4294d6245907db9f621738
nictjh <nicholastok0101@gmail.com>
2025-03-26 22:59:08 +0800

    Clean up UI

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -59,7 +59,7 @@ public class PersonCard extends UiPart<Region> {
     public PersonCard(Person person, int displayedIndex) {
         super(FXML);
         this.person = person;
-        id.setText(displayedIndex + ". ");
+        id.setText(String.valueOf(displayedIndex));
         name.setText(person.getName().fullName);
         setTextOrHide(nickname, person.getNickname(), nick -> " (" + nick + ")");
 
