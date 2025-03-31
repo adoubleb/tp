4dc1b6ccd79623af582e955f1c4a718e8289f5b4
nictjh <nicholastok0101@gmail.com>
2025-03-31 16:04:03 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -8,12 +8,9 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
-import static seedu.address.model.person.ImagePath.DEFAULT_IMAGE_RELATIVE_PATH;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BOB;
-
-import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
